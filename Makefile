f7ec1f466ada564af3910c81a1008d1df0e33725
raja moukhass <moraja858@gmail.com>
2021-04-13 11:36:26 +0000

    add box

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: amya <amya@student.42.fr>                  +#+  +:+       +#+         #
+#    By: ramoukha <ramoukha@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/22 16:14:13 by yoelguer          #+#    #+#              #
-#    Updated: 2021/04/11 20:00:09 by amya             ###   ########.fr        #
+#    Updated: 2021/04/13 10:32:15 by ramoukha         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -66,11 +66,11 @@ NAME 	= RT
 
 CFLAGS = -Wall -Wextra -Werror -g
 
-INCLUDE	= -I /Users/$$USER/.brew/Cellar/sdl2/2.0.14_1/include \
-	-I /Users/$$USER/.brew/Cellar/sdl2_image/2.0.5/include \
+INCLUDE	= -I /Users/$$USER/goinfre/.brew/Cellar/sdl2/2.0.14_1/include \
+	-I /Users/$$USER/goinfre/.brew/Cellar/sdl2_image/2.0.5/include \
 
-LIB = -L /Users/$$USER/.brew/Cellar/sdl2/2.0.14_1/lib \
-	-L   /Users/$$USER/.brew/Cellar/sdl2_image/2.0.5/lib \
+LIB = -L /Users/$$USER/goinfre/.brew/Cellar/sdl2/2.0.14_1/lib \
+	-L   /Users/$$USER/goinfre/.brew/Cellar/sdl2_image/2.0.5/lib \
 
 SDL = `sdl2-config --cflags --libs` -lSDL2 -lSDL2_image
 
--- src/box.c
+++ src/box.c
@@ -2,31 +2,40 @@
 
 t_sol intersection_box(t_obj *box, t_ray ray)
 {
-t_sol t;
-box->position = (t_vect){0,0,0};
-    t_vect  close_corner = (t_vect){box->position.x + box->size, box->position.y + box->size, box->position.z - box->size};
-    t_vect far_corner = (t_vect){box->position.x - box->size, box->position.y - box->size, box->position.z +box->size};
-t.tmin = -1;
-    double t1_x = (close_corner.x - ray.origine.x) / ray.direction.x;
-    double t2_x = (far_corner.x - ray.origine.x) / ray.direction.x;
-    double tx_min =  fmin(t1_x, t2_x);
-    double tx_max =  fmax(t1_x, t2_x);
+    t_obj plane;
+    t_vect p1;
+    t_vect d1;
+    t_vect d2;
+    double intersection;
+    t_sol t;
+    //  box->position= (t_vect){0,0,0};
+    d1 = (t_vect){0,0,1};
+    d2 = (t_vect){0,1,0};
+    plane.direction = d1;
+    p1 = add_vect(vect_mult_val(d2, box->size), box->position);
+    plane.position = p1;
 
-    double t1_y = (close_corner.y - ray.origine.y) / ray.direction.y;
-    double t2_y = (far_corner.y - ray.origine.y) / ray.direction.y;
-    double ty_min =  fmin(t1_y, t2_y);
-    double ty_max =  fmax(t1_y, t2_y);
+    t_vect hit_to_point;
+    t = intersection_ray_plan(&plane, ray);
+        plane.hit = add_vect(ray.origine, vect_mult_val(d1,t.tmin));
+    if (t.tmin != -1)
+    {
+        hit_to_point = sub_vect(p1,plane.hit);
+    intersection = vect_scal(d2, hit_to_point);
+    if (intersection < 0)
+    {
+        t.tmin = -1;
+        return (t);
+    }
+    }
 
-    double t1_z = (close_corner.z - ray.origine.z) / ray.direction.z;
-    double t2_z = (far_corner.z - ray.origine.z) / ray.direction.z;
-    double tz_min =  fmin(t1_z, t2_z);
-    double tz_max =  fmax(t1_z, t2_z);
 
-    if (fmin(fmin(tx_max, ty_max), tz_max) - fmax(fmax(tx_max, ty_max), tz_max) <= 0)
-        return t;
-        t.tmin = fmin(fmin(tx_max, ty_max), tz_max);
-    t.tmin = -1;
-    return (t);
+
+    return(t);
+
+
+
+
 
 }
 
